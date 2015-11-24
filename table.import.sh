406417eeee83dbdc7787b5e0dfe616f8c30720f8
wildknaap <eduardwildknaap@gmail.com>
2015-11-24 14:18:28 +0100

    Update table.import.sh

--- table.import.sh
+++ table.import.sh
@@ -2,24 +2,27 @@
 #
 #
 
-BASE="/path/"
-LISTFILES=`find $BASE -type f -name *.sql.gz` 
+BASE="/backup/mysql/"
+LISTFILES=`find $BASE -type f -name *.sql.gz`
 EXCLUDEDIR="mysql"
 
-for i in $LISTFILES 
+time for i in $LISTFILES
 
-	do
-	
-	DATABASE=`ls $i | cut -d "." -f1 |cut -c 15- |cut -d "/" -f1`
-	
-	TABLE=`ls $i cut -d "." -f1 |cut -c 15- |cut -d "/" -f2`	
+        do
 
-	echo $DATABASE 
-	echo $TABLE
-	echo "---"
-	
-	/bin/gunzip < $i | /usr/bin/mysql --defaults-extra-file=/etc/mysql/debian.cnf $DATABASE 
+	DATABASE=`ls $i |cut -d "." -f1 |cut -c 15- |cut -d "/" -f1`
+
+        TABLE=`ls $i |cut -d "." -f1 |cut -c 15- |cut -d "/" -f2`
+
+        echo "IMPORTING $TABLE INTO $DATABASE" 
+        echo "---"
+
+        #/bin/gunzip < $i | /usr/bin/mysql --defaults-extra-file=/etc/mysql/debian.cnf $DATABASE 
 
 done
+
+echo ""
+echo "Table import completed!"
+echo ""
 
 # EOF
